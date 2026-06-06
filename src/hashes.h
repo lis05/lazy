845ed863abfc0b72d1bae87985a028c3e5044389
Ihor Lukianov <lis05st@gmail.com>
2026-06-06 16:27:19 +0200

    add interensting idea by req for later
    Signed-off-by: Ihor Lukianov <lis05st@gmail.com>

--- src/hashes.h
+++ src/hashes.h
@@ -9,14 +9,14 @@
 #include "a5hash.h"
 
 namespace hashes {
-uint32_t static inline hash3(std::byte *data) {
+uint32_t static inline hash3(const std::byte *data) {
     return ((uint32_t)data[0] << 16) | ((uint32_t)data[1] << 8) |
            ((uint32_t)data[2]);
 }
-uint32_t static inline hash2(std::byte *data) {
+uint32_t static inline hash2(const std::byte *data) {
     return ((uint32_t)data[0] << 8) | ((uint32_t)data[1]);
 }
-uint32_t static inline hashn(std::byte *data, size_t len) {
+uint32_t static inline hashn(const std::byte *data, size_t len) {
     return a5hash(static_cast<const void *>(data), len, 0) & 0xFFFFFFFF;
 }
 }  // namespace hashes
